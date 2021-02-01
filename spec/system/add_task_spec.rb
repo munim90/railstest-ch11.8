55f06ee9f45ac0143d22fafed1c88dc625eda6cd
munim90 <munim.iftikhar@educative.io>
2021-02-01 13:16:55 +0500

    remove errors

--- spec/system/add_task_spec.rb
+++ spec/system/add_task_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe "adding a new task" do
   let!(:task_1) { create(
     :task, project: project, title: "Search Sky", size: 1, project_order: 1) }
   let!(:task_2) { create(
-    :task, project: project, title: "Use Telescope", size: 1,
-      project_order: 2) }
+    :task, project: project, title: "Use Telescope", size: 1, project_order: 2) }
   let!(:task_3) { create(
-    :task, project: project, title: "Take Notes", size: 1,
-  project_order: 3) }
-  it "can re-order a task", :js do
+    :task, project: project, title: "Take Notes", size: 1, project_order: 3) }
+  it "can re-order a task" do
     visit(project_path(project))
     find("#task_3")
     within("#task_3") do
